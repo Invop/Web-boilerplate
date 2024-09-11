01166ea3853fc1c8a11e096fee14b3d973712389
Andrii Shandryk <shandrykjj512@gmail.com>
2024-09-11 20:01:40 +0300

    Refactor favorites section and add hover effects for arrows
    Replaced carousel with favorites structure and updated arrow images. Refactored SCSS to streamline styles and enhance hover effects for control arrows. Enhanced HTML for better teacher details display with popups.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,22 @@
-const testModules = require('./test-module');
+// const testModules = require('./test-module');
 require('../css/app.css');
 
 /** ******** Your code here! *********** */
 
-console.log(testModules.hello);
+function showPopup(popupId, overlayId) {
+  document.getElementById(popupId).style.display = 'block';
+  document.getElementById(overlayId).style.display = 'block';
+}
+
+function hidePopup(popupId, overlayId) {
+  document.getElementById(popupId).style.display = 'none';
+  document.getElementById(overlayId).style.display = 'none';
+}
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('close-btn-popup')) {
+    const popupId = event.target.closest('.popup').id;
+    const overlayId = `overlay-${popupId}`;
+    hidePopup(popupId, overlayId);
+  }
+});
